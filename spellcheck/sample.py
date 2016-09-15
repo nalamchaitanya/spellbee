0c49404a9c44e7e3c4b91badad2bdfa7a708729a
rajiv <rajivpensidpri@gmail.com>
2016-09-15 22:57:25 +0530

    updated confusion sets added

--- spellcheck/sample.py
+++ spellcheck/sample.py
@@ -1,7 +1,28 @@
-
 import os
-import csv
+import spellcheck
 
 
-w = 'abcdef'
-print len(w[6:])
+f = open(spellcheck.DATA+"/ambiguos-words","r") ;
+
+l = []
+
+fl = 0
+while (fl==0) :
+    x = []
+    s = f.readline()
+    while (s=='\n') :
+        s = f.readline()
+    x.append(s.strip())
+    while (s != 'EOF') :
+        s = f.readline()
+        if (s=='\n') :
+            break
+        x.append(s.strip())
+    if (s=='EOF') :
+        fl = 1
+    l.append(x)
+
+
+l.remove(l[-1])
+for k in l :
+    print k
