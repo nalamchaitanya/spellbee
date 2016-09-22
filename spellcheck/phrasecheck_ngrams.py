dd17c01ce611c1ed9245fec504b9566c84de6373
rajiv <rajivpensidpri@gmail.com>
2016-09-22 22:20:08 +0530

    Final

--- spellcheck/phrasecheck_ngrams.py
+++ spellcheck/phrasecheck_ngrams.py
@@ -3,6 +3,49 @@ from spellcheck import *
 import  copy
 import jellyfish as jf
 
+SPELL_ERROR=1
+CONTEXT_ERROR=2
+
+
+stwords = [str(x) for x in stopwords.words('english') if len(x)<4]
+words = set([line.strip() for line in open(spellcheck.DATA+"/dict.txt",'r')])
+
+
+
+def file_as_list(f) :
+    x = f.readline()
+    l = []
+    while (len(x)!=0) :
+        l.append(x.strip())
+        x = f.readline()
+    return l
+def removestopwords(s) :
+    lis = s.split('\t')
+    for k in stwords :
+        while k in lis :
+            lis.remove(k)
+    ret = lis[0]
+    if ('the' in lis) :
+        print lis ,2
+    for k in lis[1:] :
+        ret += '\t'
+        ret += k
+    return ret
+
+f = open(DATA+'/w2_.txt','r')
+bigrams = file_as_list(f)
+bigrams_ns = [removestopwords(k) for k in  bigrams ]
+f.close()
+
+f = open(DATA+'/w3_.txt','r')
+trigrams = file_as_list(f)
+trigrams_ns = [removestopwords(k) for k in trigrams]
+f.close()
+
+f = open(DATA+'/w4_.txt','r')
+fourgrams = file_as_list(f)
+fourgrams_ns =  [removestopwords(k) for k in fourgrams]
+f.close()
 
 
 def levenshtein(s, t):
@@ -63,25 +106,25 @@ def spell_correct(phraselist,matches) :
     for i in range(len(phraselist)) :
         if phraselist[i] not in words:
             wrongword = phraselist[i]
-            #print  wrongword
             for match in matches :
                 s = copy.copy(match[1])
 
                 splitt = match[1].split('\t')
                 matching = ''.join(splitt[1:])
-                #print  wrongword,match[1]
                 if wrongword in matching :
                     sp = s.split('\t')
                     correct = ''
                     for x in sp :
-                        if x in wrongword :
+                        if (x in wrongword) & (x not in stwords):
                             correct += x+' '
                     temp = copy.copy(phraselist)
                     temp[i] = correct
                     if len(correct)!=0 :
                         suggestions.append((temp,match[0]))
                         fl = 1
-                        return suggestions
+                        break
+                if fl==1 :
+                    break
     candidates = []
     index = 0
     for i in range(len(phraselist)):
@@ -116,7 +159,7 @@ def compare_context(phraselist_nst,ngramlist) :
         fl = 0
         phonetic = jf.match_rating_codex(wd.decode('utf-8','ignore'))
         for k in baselist :
-            if (jf.levenshtein_distance(phonetic,k)<=1) :
+            if jf.levenshtein_distance(phonetic,k)<=1 :
                 fl = 1
                 break
         if (fl==0):
